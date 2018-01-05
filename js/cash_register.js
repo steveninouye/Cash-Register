ec8eebdc5d5632a861d2c5804d34a0e3ef64f878
Steven Inouye <steveninouye@msn.com>
2018-01-05 01:51:09 -1000

    finished getBalance

--- js/cash_register.js
+++ js/cash_register.js
@@ -31,7 +31,7 @@ const displayValue = function(x){
 }
 
 const addCalcHistory = function(x){
-    _total.num.push(Number(display.value));
+    _total.num.push(Number(display.value).toFixed(2));
     _total.algorithm.push(x);
     getTotal();
     var cache = `${_total.total} ${_total.algorithm[_total.algorithm.length - 1]}`;
@@ -49,19 +49,21 @@ const addCalcHistory = function(x){
             total: "",
             memory: false
         };
+        document.getElementById('history').innerHTML += 
+            _memory[_memory.length -1].total 
+            + " = <br>" +
+            eval(_memory[_memory.length -1].total).toFixed(2)
+            + "<br>";
     }
 }
 
-function getTotal(equals){
-    //resets total
+function getTotal(){
     _total.total = "";
-    //loops through all indexes of numbers and algorigthms except for last index
     for(var i = 0; i < _total.num.length - 1; i++){
         _total.total += `${_total.num[i]} ${_total.algorithm[i]} `;
     }
-    //adds last index of num to total
     _total.total += `${_total.num[_total.num.length - 1]}`;
-    document.getElementById('theTotal').innerHTML = eval(_total.total);
+    document.getElementById('theTotal').innerHTML = eval(_total.total).toFixed(2);
 };
 
 function adjustBalance(x){
@@ -69,10 +71,16 @@ function adjustBalance(x){
         if(document.getElementById('display').value !== "0"){
              _memory.push(
                 {
-                    total: document.getElementById('display').value,
+                    total: document.getElementById('display').value.toFixed(2),
                     memory: x
                 }
             )
+            document.getElementById('history').innerHTML +=
+                _memory[_memory.length -1].total 
+                + " <br>";
+            document.getElementById('display').value = 0;
+            newDisplayNeeded = true;
+            decimalInput = false;
         } else if (document.getElementById('theTotal').innerText !== "" ){
             _memory[_memory.length - 1 ].memory = x;
         } else {
@@ -81,4 +89,16 @@ function adjustBalance(x){
     } else {
         throw Error('Finish Your transaction!!!')
     }
+}
+
+function getBalance(){
+    var balance = eval(_memory.reduce((acc, curr) => {
+                if(curr.memory){
+                    acc += `${curr.memory}\(${curr.total}\)`;
+                }            
+                return acc;
+            }, "")
+        );
+    document.getElementById('calcHistoryDisplay').innerText = null;
+    document.getElementById('theTotal').innerText = balance.toFixed(2);
 }
